33feca36b001b964d545aa1c574b98474061dd6f
Riccardo Foffi <riccardo.foffi@protonmail.com>
2021-02-06 14:53:02 +0100

    fix error in definition of theta

--- src/centerofmass.jl
+++ src/centerofmass.jl
@@ -1,11 +1,12 @@
 export
     centerofmass
 
+# bai, breen 2008 journal of graphical tools
 function centerofmass(particles::P, box::AbstractVector) where P<:AbstractVector{AP} where AP<:AbstractParticle{D,T} where {D,T}
     rcom = zeros(D)
     M = size(particles, 1)
     for d in 1:D
-        θ = 2π / box[d] .* particles.r[d]
+        θ = 2π / box[d] .* [p.r[d] for p in particles]
         ξ = cos.(θ)
         ζ = sin.(θ)
         ξcom = sum(ξ) / M
@@ -22,7 +23,7 @@ function centerofmass(atoms::T, box::AbstractVector) where T<:AbstractVector{Ato
     mass = [a.m for a in atoms]
     M = sum(mass)
     for d in 1:D
-        θ = 2π / box[d] .* particles.r[d]
+        θ = 2π / box[d] .* [a.r[d] for a in atoms]
         ξ = cos.(θ)
         ζ = sin.(θ)
         ξcom = sum(ξ .* mass) / M
